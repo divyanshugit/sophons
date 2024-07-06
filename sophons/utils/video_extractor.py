9fcf660f4341bb368bdd22563d3d0e616be79bf8
Nitin A Birur <97278382+NitinAB1108@users.noreply.github.com>
2024-07-06 15:46:29 +0530

    Added video extractor script

---
+++ sophons/utils/video_extractor.py
@@ -0,0 +1,93 @@
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
+import os
+import json
+import re
+import cv2
+import pytube as pt
+import urllib.request
+import urllib
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
+client = OpenAI()
+
+def get_keyframes(transcript):
+  system_prompt = """Given the transcript of a video, you have to identify and output the 5 most important timestamps in the video which would help in summarizing the video. You need to use chain of thought reason to determine the important timestamps. Output only a json according to the given sample format. There should be no other text in the ouput other than the json.
+                      Sample Format:
+
+                      {
+                          "chain_of_thought_for_each_frame": ["reason1","reason2","reason3","reason4","reason5"]
+                          "keyframe_timestamps": [1, 2, 3, 4, 5]
+                      }
+                      """
+  completion = client.chat.completions.create(
+                model='gpt-4o',
+                messages=[{"role": "system", "content": system_prompt},
+                            {"role": "user", "content": transcript}]
+                )
+  
+  pattern = r'\{(.*?)\}'  
+  match = re.search(pattern, completion.choices[0].message.content, re.DOTALL)
+
+  try:
+    keyframes = json.loads(str('{'+match.group(1)+'}'))['keyframe_timestamps']
+  except:
+    return get_keyframes(transcript)
+  return keyframes
+
+def extract_frames_from_video(timestamps, output_folder='keyframes'):
+  if not os.path.exists(output_folder):
+      os.makedirs(output_folder)
+
+  cap = cv2.VideoCapture('temp.mp4')
+
+  if not cap.isOpened():
+      print("Error: Could not open video.")
+      return
+
+  fps = cap.get(cv2.CAP_PROP_FPS)
+
+  frame_paths = []
+  for timestamp in timestamps:
+      frame_number = int(timestamp * fps)
+      
+      cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+      
+      ret, frame = cap.read()
+      
+      if ret:
+          frame_filename = os.path.join(output_folder, f"frame_{timestamp:.2f}.png")
+          cv2.imwrite(frame_filename, frame)
+          frame_paths.append(frame_filename)
+      else:
+          print(f"Warning: Could not retrieve frame at {timestamp} seconds.")
+
+  cap.release()
+
+  return frame_paths
+
+def get_transcript(video_url):
+  video_id = video_url.split('=')[1]
+  params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % video_id}
+  url = "https://www.youtube.com/oembed"
+  query_string = urllib.parse.urlencode(params)
+  url = url + "?" + query_string
+
+  with urllib.request.urlopen(url) as response:
+      response_text = response.read()
+      data = json.loads(response_text.decode())
+      title = data['title']
+      thumbnail = data['thumbnail_url']
+
+  transcript = YouTubeTranscriptApi.get_transcript(video_id)
+  text = ' '.join([t['text'] for t in transcript])
+  yt = pt.YouTube(video_url)
+  stream = yt.streams.filter(only_audio=False)[0]
+  stream.download(filename="temp.mp4")
+  timestamps = get_keyframes(json.dumps(transcript))
+  frames = extract_frames_from_video(timestamps)
+  return title, thumbnail, text, frames
+        
